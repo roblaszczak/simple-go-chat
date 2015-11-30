72d35cb30bbce50b78c079b4385f6e54adeee6bb
Robert Laszczak <eventched@gmail.com>
2015-11-30 21:29:32 +0100

    [cmd] added logger and little refactor

--- cmd/gochat/main.go
+++ cmd/gochat/main.go
@@ -12,29 +12,39 @@ import (
 	"runtime"
 )
 
+var logger = log.New(os.Stderr, "gochat:", log.Ldate|log.Ltime)
+
 func main() {
 	RunServer(config.SERVER_HOST, config.SERVER_PORT)
 }
 
+// TODO: consider move it to separated package
 func RunServer(host string, port int) {
+	handleWebsocket()
+	handlePublicFiles()
+
+	logger.Println(fmt.Sprintf("server started at http://%s:%d/", host, port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil))
+}
+
+func handleWebsocket() {
 	websocketController := adapter.NewWebsocketController()
 	http.Handle("/chat", websocketController)
 
 	chatCore := chat.NewChat()
 	bridge := adapter.NewWebsocket(websocketController, chatCore)
 	bridge.Listen()
-
-	assertPublicDirFiles()
-	fs := http.FileServer(http.Dir(getPublicDir()))
-	http.Handle("/", fs)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil))
 }
 
-func assertPublicDirFiles() {
+func handlePublicFiles() {
 	appJsFile := getPublicDir() + "/app.js"
 	if _, err := os.Stat(appJsFile); err != nil {
 		panic(appJsFile + " doesn't exists. You need to dump JS files using 'make buildjs' command.")
 	}
+
+	fs := http.FileServer(http.Dir(getPublicDir()))
+	logger.Print(fmt.Sprintf("serving public files from %s", getPublicDir()))
+	http.Handle("/", fs)
 }
 
 func getPublicDir() string {
